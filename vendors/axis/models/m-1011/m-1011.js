5d138f95fcbeae42df1ab6c189de4e1fc9553893
Miloš Mošić <mosic.milos@gmail.com>
2015-11-04 12:42:27 +0100

    Test proposed change

--- vendors/axis/models/m-1011/m-1011.js
+++ vendors/axis/models/m-1011/m-1011.js
@@ -1,19 +1,21 @@
 {
   models: [
     {
-      id: "m1011",
-      name: "M1011",
+      id: "m1011",    // not model_id ?
+      name: "M1011",   // not model_name ?
       vendor_id: "axis",
-      username: "root",
-      password: "pass",
-      jpg_url: "axis-cgi/jpg/image.cgi",
-      h264_url: "axis-media/media.amp",
-      mjpg_url: "axis-cgi/mjpg/video.cgi",
+      vendor_name: "axis",   //add this?
+      jpg_url_primary: "axis-cgi/jpg/image.cgi",
+      h264_url_primary: "axis-media/media.amp",
+      mjpg_url_primary: "axis-cgi/mjpg/video.cgi",
+      jpg_url_secondary: "axis-cgi/jpg/image-lowres.cgi",  //add 
+      h264_url_secondary: "axis-media/media-lowres.amp",   //add 
+      mjpg_url_secondary: "axis-cgi/mjpg/video-lowres.cgi",  //add 
+      audio_url: "",
       shape: "Box",
       resolution: "640x480",
       official_url: "http://www.axis.com/products/cam_m1011/",
-      more_info: "",
-      audio_url: "",
+      more_info: "",      //Call this notes?
       poe: false,
       wifi: false,
       upnp: true,
@@ -25,27 +27,12 @@
       discontinued: true,
       onvif: true,
       psia: false,
-      defaults: {
-        auth: {
-          basic: {
-            username: "root",
-            password: "pass"
-          }
-        }
-      },
-      snapshots: {
-        h264: "axis-media/media.amp",
-        lowres: "axis-cgi/jpg/image.cgi?resolution=cif",
-        jpg: "axis-cgi/jpg/image.cgi",
-        mpeg4: "",
-        mobile: "axis-media/media.amp?resolution=QCIF",
-        mjpg: "axis-cgi/mjpg/video.cgi"
-      },
-      images: {
-        icon: "https://evercam-public-assets.s3.amazonaws.com/axis/m1011/icon.jpg",
-        thumbnail: "https://evercam-public-assets.s3.amazonaws.com/axis/m1011/thumbnail.jpg",
-        original: "https://evercam-public-assets.s3.amazonaws.com/axis/m1011/original.jpg"
-      }
+      auth_type: "basic",
+      default_username: "root",
+      default_password: "pass",
+      image_icon: "https://evercam-public-assets.s3.amazonaws.com/axis/m1011/icon.jpg",   // Incl e.g. 50x50 in name?
+      image-thumbnail: "https://evercam-public-assets.s3.amazonaws.com/axis/m1011/thumbnail.jpg",
+      image_original: "https://evercam-public-assets.s3.amazonaws.com/axis/m1011/original.jpg"
     }
   ]
 }
